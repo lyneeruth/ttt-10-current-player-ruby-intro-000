266fd2172fd827bed9257b8444d57160dd384370
Lynee Lawson <lyneelawson@gmail.com>
2021-03-06 17:52:10 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,7 @@ counter = 0
 def turn_count (board)
   board.each do |space|
     if board[space] == "X" || board[space] =="O" 
-      counter += 0 
+      counter += 1 
     end
     return counter
   end
