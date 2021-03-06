b40d171aab6f31a522dd7416df65e0136fe8d972
Lynee Lawson <lyneelawson@gmail.com>
2021-03-06 17:50:39 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,3 +1,9 @@
-
-def turn_count
+counter = 0
+def turn_count (board)
+  board.each do |space|
+    if board[space] == "X" || board[space] =="O" 
+      counter += 0 
+    end
+    return counter
+  end
   
